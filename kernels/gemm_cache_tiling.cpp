14713f9d1a1dc68227e57af9747c0a3de30c417c
Matyáš Popela <popela.matyas@student.gymzr.cz>
2026-08-18 18:44:46 +0200

    Finished 8x12 kernel. Beat Eigen on single thread performance by ~20GFLOPS/s.

--- kernels/gemm_cache_tiling.cpp
+++ kernels/gemm_cache_tiling.cpp
@@ -1,48 +1,64 @@
 #include <stdexcept>
 #include <arm_neon.h>
 #include <cstddef>
-#include "simd_micro_8x4.h"
 #include <vector>
 #include <algorithm>
 
+#include "simd_micro_8x4.h"
+#include "simd_micro_8x12.h"
 
 // at packing point Bp is (Kc, Nc)
 static inline void pack_B(int Kc, int Nc, int Nr, const float* B, int ldb, float* out) {
 
     for (size_t i = 0; i < Nc; i += Nr) {
-       for (size_t j = 0; j < Kc; ++j) {
-          float32x4_t row = vld1q_f32(&B[j * ldb + i]);
-          vst1q_f32(out, row);
-          out += Nr;
+       for (size_t j = 0; j < Kc; j ++) {
+       	float32x4_t col1 = vld1q_f32(&B[j * ldb + i + 0]);
+       	float32x4_t col2 = vld1q_f32(&B[j * ldb + i + 4]);
+       	float32x4_t col3 = vld1q_f32(&B[j * ldb + i + 8]);
+
+       	vst1q_f32(out, col1);
+       	vst1q_f32(out + 4, col2);
+       	vst1q_f32(out + 8, col3);
+       	out += 12;
        }
     }
 }
 
 // at packing point A is (Mc, Kc)
-static inline void pack_A(int Mc, int Kc, int Mr, float* A, int lda, float* out) {
+static inline void pack_A(int Mc, int Kc, int Mr, int curMr, float* A, int lda, float* out) {
 
     for (size_t i = 0; i < Mc; i += Mr) {
-       // declare ptrs 0..7
-       float* a0 = A + i * lda;
-       float* a1 = A + (i+1) * lda;
-       float* a2 = A + (i+2) * lda;
-       float* a3 = A + (i+3) * lda;
-       float* a4 = A + (i+4) * lda;
-       float* a5 = A + (i+5) * lda;
-       float* a6 = A + (i+6) * lda;
-       float* a7 = A + (i+7) * lda;
+    	if (curMr == Mr) {
+    		// declare ptrs 0..7
+    		float* a0 = A + i * lda;
+    		float* a1 = A + (i+1) * lda;
+    		float* a2 = A + (i+2) * lda;
+    		float* a3 = A + (i+3) * lda;
+    		float* a4 = A + (i+4) * lda;
+    		float* a5 = A + (i+5) * lda;
+    		float* a6 = A + (i+6) * lda;
+    		float* a7 = A + (i+7) * lda;
 
-       for (size_t k = 0; k < Kc; ++k) {
-          // march ptrs crazy style
-          *out++ = *a0++;
-          *out++ = *a1++;
-          *out++ = *a2++;
-          *out++ = *a3++;
-          *out++ = *a4++;
-          *out++ = *a5++;
-          *out++ = *a6++;
-          *out++ = *a7++;
-       }
+    		for (size_t k = 0; k < Kc; ++k) {
+    			// march ptrs crazy style
+    			*out++ = *a0++;
+    			*out++ = *a1++;
+    			*out++ = *a2++;
+    			*out++ = *a3++;
+    			*out++ = *a4++;
+    			*out++ = *a5++;
+    			*out++ = *a6++;
+    			*out++ = *a7++;
+    		}
+    	} else {
+    		for (size_t k = 0; k < Mr; ++k) {
+    			if (k <= curMr) {
+    				*out++ = A[(i+k) * lda];
+    			} else {
+    				*out++ = 0.0;
+    			}
+    		}
+    	}
     }
 }
 
@@ -54,10 +70,10 @@ void gemm_cache_tiling( int M, int K, int N,
                    float* C, int ldc) {
 
     // block size params
-    size_t Nc = 2048;
-    size_t Kc = 1024;
-    size_t Mc = 512;
-    size_t Nr = 4;
+    size_t Nc = 240;
+    size_t Kc = 256;
+    size_t Mc = 256;
+    size_t Nr = 12;
     size_t Mr = 8;
 
     // config
@@ -81,10 +97,12 @@ void gemm_cache_tiling( int M, int K, int N,
 
           for (size_t k = 0; k < M; k += Mc) {
              size_t curMc = std::min(Mc, M-k);
+          	 size_t curMr = std::min(Mr, curMc-k);
 
-             pack_A(curMc, curKc, Mr, &A[k*lda+j], lda, packed_A);
+             pack_A(curMc, curKc, Mr, curMr, &A[k*lda+j], lda, packed_A);
 
              for (size_t l = 0; l < curNc; l += Nr) {
+             	size_t curNr = std::min(Nr, curNc-l);
 
                 float* ptr_B = packed_B + l * curKc;
 
@@ -93,7 +111,7 @@ void gemm_cache_tiling( int M, int K, int N,
                    float* ptr_A = packed_A + m * curKc;
                    float* ptr_C = &C[m*ldc + k*ldc + l + i];
 
-                   simd_micro_8x4(Nr, curKc, Mr, ptr_B, ptr_A, ptr_C, ldc, is_first_k_block);
+                   simd_micro_8x12(curNr, curKc, curMr, ptr_B, ptr_A, ptr_C, ldc, is_first_k_block);
 
                 }
              }
--- kernels/simd_micro_8x4.h
+++ kernels/simd_micro_8x4.h
@@ -4,8 +4,8 @@
 #include <arm_neon.h>
 
 // A, B are pre packed into contiguous buffers
-void simd_micro_8x4(	int Nr, int Kc, int Mr, float* B,
-						float* A, float* C, int ldc, bool is_first_k_block) {
+void simd_micro_8x4(	int Nr, int Kc, int Mr, float* __restrict B,
+						float* __restrict A, float* __restrict C, int ldc, bool is_first_k_block) {
 
 	float32x4_t c0 = vdupq_n_f32(0.f);
 	float32x4_t c1 = vdupq_n_f32(0.f);
@@ -16,28 +16,68 @@ void simd_micro_8x4(	int Nr, int Kc, int Mr, float* B,
 	float32x4_t c6 = vdupq_n_f32(0.f);
 	float32x4_t c7 = vdupq_n_f32(0.f);
 
-	for (int i = 0; i < Kc; i ++) {
+	for (int i = 0; i < Kc; i += 4) {
 
-		float32x4_t a0 = vdupq_n_f32(A[0 + i*Mr]);
-		float32x4_t a1 = vdupq_n_f32(A[1 + i*Mr]);
-		float32x4_t a2 = vdupq_n_f32(A[2 + i*Mr]);
-		float32x4_t a3 = vdupq_n_f32(A[3 + i*Mr]);
-		float32x4_t a4 = vdupq_n_f32(A[4 + i*Mr]);
-		float32x4_t a5 = vdupq_n_f32(A[5 + i*Mr]);
-		float32x4_t a6 = vdupq_n_f32(A[6 + i*Mr]);
-		float32x4_t a7 = vdupq_n_f32(A[7 + i*Mr]);
+		float32x4_t a1 = vld1q_f32(A + 0);
+		float32x4_t a2 = vld1q_f32(A + 4);
 
-		float32x4_t b = vld1q_f32(B);
-		B+=Nr;
+		float32x4_t b = vld1q_f32(B + 0);
 
-		c0 = vfmaq_f32(c0, a0, b);
-		c1 = vfmaq_f32(c1, a1, b);
-		c2 = vfmaq_f32(c2, a2, b);
-		c3 = vfmaq_f32(c3, a3, b);
-		c4 = vfmaq_f32(c4, a4, b);
-		c5 = vfmaq_f32(c5, a5, b);
-		c6 = vfmaq_f32(c6, a6, b);
-		c7 = vfmaq_f32(c7, a7, b);
+		c0 = vfmaq_laneq_f32(c0, b, a1, 0);
+		c1 = vfmaq_laneq_f32(c1, b, a1, 1);
+		c2 = vfmaq_laneq_f32(c2, b, a1, 2);
+		c3 = vfmaq_laneq_f32(c3, b, a1, 3);
+		c4 = vfmaq_laneq_f32(c4, b, a2, 0);
+		c5 = vfmaq_laneq_f32(c5, b, a2, 1);
+		c6 = vfmaq_laneq_f32(c6, b, a2, 2);
+		c7 = vfmaq_laneq_f32(c7, b, a2, 3);
+
+		a1 = vld1q_f32(A + 8);
+		a2 = vld1q_f32(A + 12);
+
+		b = vld1q_f32(B + 4);
+
+
+		c0 = vfmaq_laneq_f32(c0, b, a1, 0);
+		c1 = vfmaq_laneq_f32(c1, b, a1, 1);
+		c2 = vfmaq_laneq_f32(c2, b, a1, 2);
+		c3 = vfmaq_laneq_f32(c3, b, a1, 3);
+		c4 = vfmaq_laneq_f32(c4, b, a2, 0);
+		c5 = vfmaq_laneq_f32(c5, b, a2, 1);
+		c6 = vfmaq_laneq_f32(c6, b, a2, 2);
+		c7 = vfmaq_laneq_f32(c7, b, a2, 3);
+
+		a1 = vld1q_f32(A + 16);
+		a2 = vld1q_f32(A + 20);
+
+		b = vld1q_f32(B + 8);
+
+		c0 = vfmaq_laneq_f32(c0, b, a1, 0);
+		c1 = vfmaq_laneq_f32(c1, b, a1, 1);
+		c2 = vfmaq_laneq_f32(c2, b, a1, 2);
+		c3 = vfmaq_laneq_f32(c3, b, a1, 3);
+		c4 = vfmaq_laneq_f32(c4, b, a2, 0);
+		c5 = vfmaq_laneq_f32(c5, b, a2, 1);
+		c6 = vfmaq_laneq_f32(c6, b, a2, 2);
+		c7 = vfmaq_laneq_f32(c7, b, a2, 3);
+
+		a1 = vld1q_f32(A + 24);
+		a2 = vld1q_f32(A + 28);
+
+		b = vld1q_f32(B + 12);
+
+		c0 = vfmaq_laneq_f32(c0, b, a1, 0);
+		c1 = vfmaq_laneq_f32(c1, b, a1, 1);
+		c2 = vfmaq_laneq_f32(c2, b, a1, 2);
+		c3 = vfmaq_laneq_f32(c3, b, a1, 3);
+		c4 = vfmaq_laneq_f32(c4, b, a2, 0);
+		c5 = vfmaq_laneq_f32(c5, b, a2, 1);
+		c6 = vfmaq_laneq_f32(c6, b, a2, 2);
+		c7 = vfmaq_laneq_f32(c7, b, a2, 3);
+
+		A += 32;
+		B += 16;
+
 	}
 
 	if (is_first_k_block) {
